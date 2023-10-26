d39461615992c2cc767c6bbf55b29d5db09e0d64
hussenmi <hussenmohammedak@gmail.com>
2023-10-26 07:45:25 -0400

    Added easy access to site after updates

--- rollback_to_v2.sh
+++ rollback_to_v2.sh
@@ -2,10 +2,10 @@
 
 echo "Rolling back to v2..."
 kubectl rollout undo deployment/flask-deployment
-sleep 15  # Give some time for the rollback to stabilize
+sleep 10  # Give some time for the rollback to stabilize
 
 echo "Accessing the rolled back service..."
-minikube service flask-service --url | xargs curl
+minikube service flask-service --url
 echo
 
 echo "Rollback to v2 complete!"
