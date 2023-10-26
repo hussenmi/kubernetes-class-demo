45039471a3ea9c952fd33a3db07793a48903fb6c
Hussen Mohammed Ibrahim <hussenmohammedak@gmail.com>
2023-10-26 19:15:58 +0800

    Deleted sime files

--- update_to_v2.sh
+++ update_to_v2.sh
@@ -1,15 +1,29 @@
+# #!/bin/bash
+
+# echo "Building the Docker image for v2..."
+# # docker build -t my-flask-app:v2 .
+# docker build --no-cache -t my-flask-app:v2 .
+
+# echo "Updating the application to v2..."
+# kubectl set image deployment/flask-deployment flask-app-container=my-flask-app:v2
+# sleep 15  # Give some time for the update to stabilize
+
+# echo "Accessing the updated service..."
+# minikube service flask-service --url | xargs curl
+# echo
+
+# echo "Update to v2 complete!"
+
 #!/bin/bash
 
 echo "Building the Docker image for v2..."
-# docker build -t my-flask-app:v2 .
 docker build --no-cache -t my-flask-app:v2 .
 
 echo "Updating the application to v2..."
 kubectl set image deployment/flask-deployment flask-app-container=my-flask-app:v2
 sleep 15  # Give some time for the update to stabilize
 
-echo "Accessing the updated service..."
-minikube service flask-service --url | xargs curl
-echo
+echo "The application has been updated to v2. You can access it using the following URL:"
+minikube service flask-service --url
 
 echo "Update to v2 complete!"
